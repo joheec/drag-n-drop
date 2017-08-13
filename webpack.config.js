aae4f1d2a10685549a14323bda6a207c3fd287f0
Johee Chung <johee@candescenthealth.com>
2017-08-12 21:51:57 -0500

    Implement webpack to bundle js file.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/renderer.js',
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'renderer.bundle.js',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
